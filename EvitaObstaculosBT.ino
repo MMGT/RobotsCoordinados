5685fe5075339e10181cbb14b4d81ef6ae9896f2
MMGT <41291602+MMGT@users.noreply.github.com>
2018-07-16 12:43:20 +0100

    Add files via upload

---
+++ EvitaObstaculosBT.ino
@@ -0,0 +1,68 @@
+#include <SR04.h>
+#include <Servo.h>
+#include <SoftwareSerial.h>
+
+SoftwareSerial BT(2,4);
+Servo Izquierdo;
+Servo Derecho;
+SR04 detector = SR04(4,5);
+
+void setup() {
+  Serial.begin(9600);
+  BT.begin(9600);
+  Izquierdo.attach(8);
+  Derecho.attach(9);
+
+}
+
+void loop(){
+  long a = detector.Distance();
+  if (a < 30){
+    parar();
+    BT.write('s');
+    delay(100);
+  }else{
+    BT.write('o');
+    if (BT.available()){
+      char c = BT.read();
+      switch (c){
+        case 'u':
+          adelante();
+          break;
+        case 'd':
+          atras();
+          break;
+        case 'l':
+          izquierda();
+          break;
+        case 'r':
+          derecha();
+          break;
+        default:
+          break;
+      }
+    }
+    delay(10);
+  }
+}
+
+void adelante(){
+  
+}
+
+void atras(){
+  
+}
+
+void izquierda(){
+  
+}
+
+void derecha(){
+  
+}
+
+void parar(){
+  
+}
+
---
+++ SigueLineasBT.ino
@@ -0,0 +1,70 @@
+#include <Servo.h>
+#include <SoftwareSerial.h>
+
+int sigueI = 6;
+int sigueD = 7;
+char caminar = 'c';
+SoftwareSerial BT(2,4);
+Servo Izquierdo;
+Servo Derecho;
+
+void setup() {
+  Serial.begin(9600);
+  BT.begin(9600);
+  Izquierdo.attach(8);
+  Derecho.attach(9);
+  pinMode(sigueI,INPUT);
+  pinMode(sigueD,INPUT);
+}
+
+void loop() {
+  if (BT.available()){
+    caminar = BT.read();
+    delay(50);
+  }
+  if(caminar =='s'){//Si me ordenan parar espero stop
+    parar();
+    delay(50)
+  }else{//Si recibo c de clear es que el camino está despejado
+    bool iz = digitalRead(sigueI);
+    bool de = digitalRead(sigueD);
+    if (iz && de){
+      adelante();
+      BT.write("u");
+      delay(50);
+    }else if (iz && !de){
+      izquierda();
+      BT.write("l");
+      delay(50);
+    }else if (!iz && de){
+      derecha();
+      BT.write("r");
+      delay(50);
+    }else{
+      atras();
+      BT.write("d");
+      delay(50);
+    }
+  }
+}
+
+void adelante(){
+  
+}
+
+void atras(){
+  
+}
+
+void izquierda(){
+  
+}
+
+void derecha(){
+  
+}
+
+void parar(){
+  
+}
+
